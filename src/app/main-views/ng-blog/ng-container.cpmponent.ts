74ffb2164a89f8ac17489b6ed16cda585fddfcf6
xinyang3 <xinyang.zhu@outlook.com>
2019-08-07 19:01:03 +0800

    angular blogs route heroes

---
+++ src/app/main-views/ng-blog/ng-container.cpmponent.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: '.ng-containter',
+    templateUrl: './ng-container.component.html',
+    styleUrls: ['./ng-container.component.scss']
+})
+export class NgContainerComponent {
+    constructor () {
+
+    }
+    name = 'ng samples';
+}
